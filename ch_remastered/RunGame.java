12f54deecdb05c30a4b0118af0748f9c7774de5e
Amanda Onyeaso <nannchi@unc.edu>
2025-03-04 21:18:18 -0500

    trying to fix what I just broke?

--- ch_remastered/RunGame.java
+++ ch_remastered/RunGame.java
@@ -1,15 +1,27 @@
-import java.util.ArrayList;
-import java.io.FileWriter;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class RunGame {
-    ArrayList<Save> _saves = new ArrayList<Save>();
-    static String csvFilePath = "ch_remastered/DATA/SAVEDATA.csv";
+    public String curSave;
     public static void main(String[] args) throws IOException {
-        FileWriter writer = new FileWriter(csvFilePath);
-        writer.write("yo");
-        writer.close();
+        Scanner userInput = new Scanner(System.in);
+        newSave(userInput.nextLine());
+        userInput.close();
          
+    }
+
+    public static String newSave(String name) throws IOException {
+        File newSave = new File("ch_remastered/DATA"+"/"+name+".txt");        
+        if (!newSave.exists()) {
+            newSave.createNewFile();
+            return "ch_remastered/DATA"+"/"+name+".txt";
+        }
+        else {
+            System.out.println("ERROR");
+            return "ch_remastered/DATA"+"/"+name+".txt";
+        }
     }
 
 }
