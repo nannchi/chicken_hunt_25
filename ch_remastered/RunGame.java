f71236a74c6b033508b4a5e733290fb169e26974
Amanda Onyeaso <nannchi@unc.edu>
2025-03-06 15:08:06 -0500

    Concepts of a Save system implemented

--- ch_remastered/RunGame.java
+++ ch_remastered/RunGame.java
@@ -7,20 +7,20 @@ public class RunGame {
     public String curSave;
     public static void main(String[] args) throws IOException {
         Scanner userInput = new Scanner(System.in);
-        newSave(userInput.nextLine());
+        System.out.println(newSave(userInput.nextLine()) + " Created");
         userInput.close();
          
     }
 
     public static String newSave(String name) throws IOException {
-        File newSave = new File("ch_remastered/DATA"+"/"+name+".txt");        
+        File newSave = new File("ch_remastered/SAVEDATA"+"/"+name+".txt");        
         if (!newSave.exists()) {
             newSave.createNewFile();
-            return "ch_remastered/DATA"+"/"+name+".txt";
+            return "ch_remastered/SAVEDATA"+"/"+name+".txt";
         }
         else {
-            System.out.println("ERROR");
-            return "ch_remastered/DATA"+"/"+name+".txt";
+            System.out.println("Save exists");
+            return "ch_remastered/SAVEDATA"+"/"+name+".txt";
         }
     }
 
